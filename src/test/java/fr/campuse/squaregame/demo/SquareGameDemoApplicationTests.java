ee61173037c7bad3ff8cbf7c32c0c2c17a7ff08e
Théo <theo.cartegnie@le-campus-numerique.fr>
2025-11-14 16:44:25 +0100

    Add new request : - DELETE - PUT - GET
    work with Postman

---
+++ src/test/java/fr/campuse/squaregame/demo/SquareGameDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campuse.squaregame.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGameDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
